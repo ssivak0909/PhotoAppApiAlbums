7fc3a9bccc528062da004a8d11fe87bcbfa93f56
Sivakrishna Sidda <Sivakrishna_Sidda@epam.com>
2022-08-26 13:36:46 +0530

    added new end point

--- src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/AlbumsController.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/AlbumsController.java
@@ -8,12 +8,17 @@ package com.appsdeveloperblog.photoapp.api.albums.io.controllers;
 import com.appsdeveloperblog.photoapp.api.albums.data.AlbumEntity;
 import com.appsdeveloperblog.photoapp.api.albums.service.AlbumsService;
 import com.appsdeveloperblog.photoapp.api.albums.ui.model.AlbumResponseModel;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import org.modelmapper.ModelMapper;
+
 import java.lang.reflect.Type;
+
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,30 +26,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 @RestController
-@RequestMapping("/users/{id}/albums")
+@RequestMapping("/users")
 public class AlbumsController {
-    
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/env")
+    public String displayEnvVal() {
+        return String.format("you are on %s environment",env);
+    }
+
     @Autowired
     AlbumsService albumsService;
     Logger logger = LoggerFactory.getLogger(this.getClass());
-    @GetMapping( 
-            produces = { 
-                MediaType.APPLICATION_JSON_VALUE
+
+    @GetMapping(value = "/{id}/albums",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE
             })
     public List<AlbumResponseModel> userAlbums(@PathVariable String id) {
 
         List<AlbumResponseModel> returnValue = new ArrayList<>();
-        
+
         List<AlbumEntity> albumsEntities = albumsService.getAlbums(id);
-        
-        if(albumsEntities == null || albumsEntities.isEmpty())
-        {
+
+        if (albumsEntities == null || albumsEntities.isEmpty()) {
             return returnValue;
         }
-        
-        Type listType = new TypeToken<List<AlbumResponseModel>>(){}.getType();
- 
+
+        Type listType = new TypeToken<List<AlbumResponseModel>>() {
+        }.getType();
+
         returnValue = new ModelMapper().map(albumsEntities, listType);
         logger.info("Returning " + returnValue.size() + " albums");
         return returnValue;
